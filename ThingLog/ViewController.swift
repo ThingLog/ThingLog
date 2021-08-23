4c30dc4c96e0ba7497bc741467b8e233d6ac84a7
JiWon Lee <jiwon.swdev@gmail.com>
2021-08-23 17:14:17 +0900

    Initial Commit

---
+++ ThingLog/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ThingLog
+//
+//  Created by 이지원 on 2021/08/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
