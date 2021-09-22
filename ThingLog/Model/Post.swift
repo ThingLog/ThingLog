2c7e2199eafba3e0ff17f09e4dd342c7cab360d3
JiWon Lee <jiwon.swdev@gmail.com>
2021-09-22 21:51:37 +0900

    [#2] feat: Post 구조체 생성

---
+++ ThingLog/Model/Post.swift
@@ -0,0 +1,16 @@
+//
+//  Post.swift
+//  ThingLog
+//
+//  Created by 이지원 on 2021/09/22.
+//
+
+import Foundation
+
+struct Post {
+    let title: String
+    let price: Int
+    let purchasePlace: String?
+    let purchaseDate: Date?
+    let contents: String?
+}
