00b97fac4dbf3c603cab8d8134d070291de8d7af
gustn3965 <gustn3965@gmail.com>
2021-09-22 17:36:39 +0900

    [#15] view: HomeViewController에 필요한 view 배치

--- ThingLog/ViewController/HomeViewController.swift
+++ ThingLog/ViewController/HomeViewController.swift
@@ -10,9 +10,62 @@ import UIKit
 final class HomeViewController: UIViewController {
     var coordinator: Coordinator?
     
+    var contentsContainerView: UIView = {
+        let view: UIView = UIView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+    
+    var contentsTabView: ContentsTabView = {
+        let view: ContentsTabView = ContentsTabView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+    
+    let pageViewController: ContentsPageViewController = {
+        let controller: ContentsPageViewController = ContentsPageViewController(transitionStyle: .scroll, navigationOrientation: .horizontal, options: nil)
+        return controller
+    }()
+    
     // MARK: - Life cycle
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .white
+        setupContainerView()
+        setupContentsTabView()
+        setupPageViewController()
+    }
+    
+    func setupContainerView() {
+        view.addSubview(contentsContainerView)
+        NSLayoutConstraint.activate([
+            contentsContainerView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            contentsContainerView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            contentsContainerView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 100),
+            contentsContainerView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor)
+        ])
+    }
+    
+    func setupContentsTabView() {
+        contentsContainerView.addSubview(contentsTabView)
+        NSLayoutConstraint.activate([
+            contentsTabView.leadingAnchor.constraint(equalTo: contentsContainerView.leadingAnchor),
+            contentsTabView.trailingAnchor.constraint(equalTo: contentsContainerView.trailingAnchor),
+            contentsTabView.topAnchor.constraint(equalTo: contentsContainerView.safeAreaLayoutGuide.topAnchor),
+            contentsTabView.heightAnchor.constraint(equalToConstant: 36)
+        ])
+    }
+    
+    func setupPageViewController() {
+        addChild(pageViewController)
+        contentsContainerView.addSubview(pageViewController.view)
+        let pageView: UIView = pageViewController.view
+        pageView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            pageView.leadingAnchor.constraint(equalTo: contentsContainerView.leadingAnchor),
+            pageView.trailingAnchor.constraint(equalTo: contentsContainerView.trailingAnchor),
+            pageView.topAnchor.constraint(equalTo: contentsTabView.bottomAnchor),
+            pageView.bottomAnchor.constraint(equalTo: contentsContainerView.bottomAnchor)
+        ])
     }
 }
