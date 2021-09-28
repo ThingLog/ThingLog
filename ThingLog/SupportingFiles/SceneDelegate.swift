938dbb97a5335f3329cf0394a0e5f3d0bf28e855
gustn3965 <gustn3965@gmail.com>
2021-09-29 00:41:42 +0900

    [#38] ref: HorizontalCollectionView - 애니메이션 제거하고 sectionInset으로 리팩토링

--- ThingLog/SupportingFiles/SceneDelegate.swift
+++ ThingLog/SupportingFiles/SceneDelegate.swift
@@ -16,7 +16,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
         guard let scene = (scene as? UIWindowScene) else { return }
         window = UIWindow(windowScene: scene)
-        window?.rootViewController = TabBarController()
+        window?.rootViewController = ViewController()
         window?.makeKeyAndVisible()
     }
 
--- ThingLog/ViewController.swift
+++ ThingLog/ViewController.swift
@@ -8,9 +8,22 @@
 import UIKit
 
 class ViewController: UIViewController {
+    var horizontalView: HorizontalCollectionView = {
+        let view =  HorizontalCollectionView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.backgroundColor = .black
+        view.addSubview(horizontalView)
+        horizontalView.categoryList = ["학용품","다이어리","학용품","다이어리","학용품","다이어리","학용품","다이어리", "학용품","다이어리","학용품","다이어리","학용품","다이어리","학용품","다이어리"]
+        NSLayoutConstraint.activate([
+            horizontalView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            horizontalView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            horizontalView.topAnchor.constraint(equalTo: view.topAnchor, constant: 100),
+            horizontalView.heightAnchor.constraint(equalToConstant: 44)
+        ])
         // Do any additional setup after loading the view.
     }
 }
-
