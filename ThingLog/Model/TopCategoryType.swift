9a7b1b2a609af6d8df45395f876392abc196f84c
gustn3965 <gustn3965@gmail.com>
2021-09-28 19:56:04 +0900

    [#24] env: TopCategoryType 객체 구현

---
+++ ThingLog/Model/TopCategoryType.swift
@@ -0,0 +1,25 @@
+//
+//  TopCategoryType.swift
+//  ThingLog
+//
+//  Created by hyunsu on 2021/09/28.
+//
+
+import Foundation
+
+
+/// 모아보기 - 최상단에 있는 카테고리 탭에 필요한 데이터를 추상화한 객체다.
+enum TopCategoryType {
+    case total, date, category, like, preference, price
+    
+    var filterTypes: [FilterType] {
+        switch self {
+        case .like, .preference, .price:
+            return [.preference]
+        case .total, .category:
+            return [.latest]
+        case .date:
+            return [.year, .month, .latest]
+        }
+    }
+}
