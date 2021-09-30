674607ea01a1fdfc3fbb021af3d930c6cb152814
gustn3965 <gustn3965@gmail.com>
2021-09-30 14:39:28 +0900

    [#39] ref: TopCategoryType 수정

--- ThingLog/Model/TopCategoryType.swift
+++ ThingLog/Model/TopCategoryType.swift
@@ -10,8 +10,13 @@ import Foundation
 
 /// 모아보기 - 최상단에 있는 카테고리 탭에 필요한 데이터를 추상화한 객체다.
 /// 각 탭에 대하여 필요한 `FilterType`들을 제공한다.
-enum TopCategoryType {
-    case total, date, category, like, preference, price
+enum TopCategoryType: String, CaseIterable {
+    case total = "전체"
+    case date = "년도"
+    case category = "카테고리"
+    case like = "좋아요"
+    case preference = "만족도"
+    case price = "가격"
     
     var filterTypes: [FilterType] {
         switch self {
