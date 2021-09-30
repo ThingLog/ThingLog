7ed80244e805073be351c92bca18178980de2015
gustn3965 <gustn3965@gmail.com>
2021-09-30 14:44:07 +0900

    [#39] view: CategoryTypeButton 추가

---
+++ ThingLog/View/CategoryTypeButton.swift
@@ -0,0 +1,23 @@
+//
+//  CategoryTypeButton.swift
+//  ThingLog
+//
+//  Created by hyunsu on 2021/09/30.
+//
+
+import UIKit
+
+// CategoryTapView에 들어가는 Button에 TopCategoryType을 프로퍼티로 가지는 UIButton이다.
+final class CategoryTypeButton: UIButton {
+    var type: TopCategoryType
+    
+    init(type: TopCategoryType) {
+        self.type = type
+        super.init(frame: .zero)
+    }
+    
+    required init?(coder: NSCoder) {
+        self.type = .category
+        super.init(coder: coder)
+    }
+}
