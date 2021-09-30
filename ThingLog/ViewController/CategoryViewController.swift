ac8e1000f0711b942033cf0f3813fbdbaeaaf9fa
gustn3965 <gustn3965@gmail.com>
2021-09-30 15:46:21 +0900

    [#44] view: 모아보기 네비게이션 바 구현

--- ThingLog/ViewController/CategoryViewController.swift
+++ ThingLog/ViewController/CategoryViewController.swift
@@ -10,18 +10,46 @@ import UIKit
 final class CategoryViewController: UIViewController {
     var coordinator: Coordinator?
     
-    // MARK: - Life cycle 
+    // MARK: - Life cycle
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = SwiftGenColors.white.color
+        setupNavigationBar()
+    }
+    
+    func setupNavigationBar() {
+        if #available(iOS 15, *) {
+            let appearance: UINavigationBarAppearance = UINavigationBarAppearance()
+            appearance.configureWithOpaqueBackground()
+            appearance.backgroundColor = SwiftGenColors.white.color
+            appearance.shadowColor = .clear
+            navigationController?.navigationBar.standardAppearance = appearance
+            navigationController?.navigationBar.scrollEdgeAppearance = navigationController?.navigationBar.standardAppearance
+        } else {
+            navigationController?.navigationBar.isTranslucent = false
+            navigationController?.navigationBar.barTintColor = SwiftGenColors.white.color
+            navigationController?.navigationBar.setValue(true, forKey: "hidesShadow")
+        }
         
-        // Test 
-        let label: UILabel = UILabel(frame: CGRect(origin: .zero, size: CGSize(width: 100, height: 100)))
-        label.center = view.center
-        label.backgroundColor = SwiftGenColors.white.color
-        label.textColor = SwiftGenColors.black.color
-        label.textAlignment = .center
-        label.text = "Category"
-        view.addSubview(label)
+        let logoView: UILabel = UILabel()
+        logoView.text = "모아보기"
+        logoView.textColor = SwiftGenColors.black.color
+        logoView.font = UIFont.Pretendard.headline3
+        let logoBarButtonItem: UIBarButtonItem = UIBarButtonItem(customView: logoView)
+        navigationItem.leftBarButtonItem = logoBarButtonItem
+        
+        let searchButton: UIButton = UIButton()
+        searchButton.setImage(SwiftGenAssets.search.image, for: .normal)
+        searchButton.tintColor = SwiftGenColors.black.color
+        
+        let settingButton: UIButton = UIButton()
+        settingButton.setImage(SwiftGenAssets.setting.image, for: .normal)
+        settingButton.tintColor = SwiftGenColors.black.color
+        // settingButton.addTarget(self, action: #selector(showSettingView), for: .touchUpInside)
+        let settingBarButton: UIBarButtonItem = UIBarButtonItem(customView: settingButton)
+        let searchBarButton: UIBarButtonItem = UIBarButtonItem(customView: searchButton)
+        let spacingBarButton: UIBarButtonItem = UIBarButtonItem(barButtonSystemItem: .fixedSpace, target: nil, action: nil)
+        spacingBarButton.width = 24
+        navigationItem.rightBarButtonItems = [settingBarButton, spacingBarButton, searchBarButton]
     }
 }
