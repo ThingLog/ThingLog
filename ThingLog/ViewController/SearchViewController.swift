9174e575cca300b633664a4ba005fb53a9c64799
gustn3965 <gustn3965@gmail.com>
2021-10-02 18:33:08 +0900

    [#52] view: SearchController 생성

---
+++ ThingLog/ViewController/SearchViewController.swift
@@ -0,0 +1,17 @@
+//
+//  SearchViewController.swift
+//  ThingLog
+//
+//  Created by hyunsu on 2021/10/02.
+//
+
+import UIKit
+
+/// 검색화면을 보여주는 ViewController다.
+final class SearchViewController: UIViewController {
+    var coordinator: CategoryCoordinator?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
