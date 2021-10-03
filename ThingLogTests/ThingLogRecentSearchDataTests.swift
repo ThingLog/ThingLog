76c8b7f6b1726030233debab65b7d1724eb90224
gustn3965 <gustn3965@gmail.com>
2021-10-03 18:34:34 +0900

    [#48] feat: ViewModel 구현 - RecentSearchDataViewModel 구현 - ThingLogRecentSearchDataTests 테스트코드 작성

---
+++ ThingLogTests/ThingLogRecentSearchDataTests.swift
@@ -0,0 +1,68 @@
+    //
+//  ThingLogRecentSearchDataTests.swift
+//  ThingLogTests
+//
+//  Created by hyunsu on 2021/10/03.
+//
+
+import XCTest
+@testable import ThingLog
+import RxSwift
+    
+class ThingLogRecentSearchDataTests: XCTestCase {
+
+    var recentViewModel: RecentSearchDataViewModel = RecentSearchDataViewModel()
+    var disposeBag: DisposeBag = DisposeBag()
+
+    func test_최근검색어_데이터를_하나추가하면_subject가_변경된다() throws {
+        recentViewModel.removeAll()
+        var count: Int = 0
+        timeout(3) { exp in
+            recentViewModel.recentSearchDataSubject
+                .bind { val in
+                    count += 1
+                    if count == 2 {
+                        exp.fulfill()
+                        XCTAssertTrue(val.count == 1 )
+                    }
+                }
+                .disposed(by: disposeBag)
+            
+            self.recentViewModel.add("hiii")
+        }
+    }
+    
+    func test_최근검색어_데이터를_하나추가하고_하나를_삭제해도_subject가_변경된다() throws {
+        recentViewModel.removeAll()
+        var count: Int = 0
+        timeout(3) { exp in
+            recentViewModel.recentSearchDataSubject
+                .bind { val in
+                    count += 1
+                    if count == 3 {
+                        exp.fulfill()
+                        XCTAssertTrue(val.count == 0 )
+                    }
+                }
+                .disposed(by: disposeBag)
+            
+            self.recentViewModel.add("hiii")
+            self.recentViewModel.remove(0)
+        }
+    }
+    
+    func test_같은_최근검색어는_추가되지_않는다() throws {
+        recentViewModel.removeAll()
+        recentViewModel.add("hi")
+        recentViewModel.add("hi")
+        
+        XCTAssertTrue((UserDefaults.standard.value(forKey: UserDefaults.recentSearchData) as? [String])?.count == 1)
+    }
+    
+    func test_최근검색어_삭제할수있다() {
+        recentViewModel.removeAll()
+        recentViewModel.add("hi")
+        recentViewModel.remove(0)
+        XCTAssertTrue((UserDefaults.standard.value(forKey: UserDefaults.recentSearchData) as? [String])?.count == 0)
+    }
+}
