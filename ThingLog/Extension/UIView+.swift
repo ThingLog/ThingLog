9ec0dc0e0321c694a6b9a3f7c0ffd505c1848e25
JiWon Lee <jiwon.swdev@gmail.com>
2021-10-03 15:44:11 +0900

    [#56] feat: findParentViewController() in UIView+

--- ThingLog/Extension/UIView+.swift
+++ ThingLog/Extension/UIView+.swift
@@ -11,4 +11,16 @@ extension UIView {
     static var reuseIdentifier: String {
         String(describing: Self.self)
     }
+
+    /// 현재 뷰의 부모 뷰 컨트롤러를 찾는다.
+    /// - Returns: 현재 뷰의 부모 뷰 컨트롤러(optional)를 반환한다. 
+    func findParentViewController() -> UIViewController? {
+        if let nextResponder: UIViewController = self.next as? UIViewController {
+            return nextResponder
+        } else if let nextResponder: UIView = self.next as? UIView {
+            return nextResponder.findParentViewController()
+        } else {
+            return nil
+        }
+    }
 }
