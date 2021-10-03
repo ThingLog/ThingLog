676559e0ff6aa99a217e1596b1749d0f36305cb6
JiWon Lee <jiwon.swdev@gmail.com>
2021-10-03 15:52:41 +0900

    [#56] feat: WriteCoordinator 구현

---
+++ ThingLog/Coordinator/WriteCoordinator.swift
@@ -0,0 +1,34 @@
+//
+//  WriteCoordinator.swift
+//  ThingLog
+//
+//  Created by 이지원 on 2021/10/03.
+//
+
+import UIKit
+
+final class WriteCoordinator: Coordinator {
+    var childCoordinators: [Coordinator] = []
+    var navigationController: UINavigationController
+    private weak var parentViewController: UITabBarController?
+
+    init(navigationController: UINavigationController,
+         parentViewController: UITabBarController?) {
+        self.navigationController = navigationController
+        self.parentViewController = parentViewController
+    }
+
+    func start() {
+        showWriteViewController(with: .bought)
+    }
+
+    /// WriteViewController로 이동한다.
+    /// - Parameter type: WriteType에 따라 ViewModel을 생성하기 위해 필요하다.
+    func showWriteViewController(with type: WriteType) {
+        let writeViewController: WriteViewController = WriteViewController()
+        writeViewController.coordinator = self
+        navigationController.modalPresentationStyle = .fullScreen
+        navigationController.pushViewController(writeViewController, animated: true)
+        parentViewController?.present(navigationController, animated: true)
+    }
+}
