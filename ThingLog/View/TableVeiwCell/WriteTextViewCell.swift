db7b459e2e0001fefeee27aa2d5c22212ff5246e
JiWon Lee <jiwon.swdev@gmail.com>
2021-10-11 15:49:01 +0900

    [#82] view: WriteTextViewCell

---
+++ ThingLog/View/TableVeiwCell/WriteTextViewCell.swift
@@ -0,0 +1,81 @@
+//
+//  WriteTextViewCell.swift
+//  ThingLog
+//
+//  Created by 이지원 on 2021/10/11.
+//
+
+import UIKit
+
+protocol WriteTextViewCellDelegate: AnyObject {
+    func updateTextViewHeight(_ cell: WriteTextViewCell, _ textView: UITextView)
+}
+
+final class WriteTextViewCell: UITableViewCell {
+    private let textView: UITextView = {
+        let textView: UITextView = UITextView()
+        textView.translatesAutoresizingMaskIntoConstraints = false
+        textView.isEditable = true
+        textView.isScrollEnabled = false
+        textView.font = UIFont.Pretendard.body1
+        textView.textColor = SwiftGenColors.gray4.color
+        textView.text = "물건에 대한 생각이나 감정을 자유롭게 기록해보세요."
+        textView.sizeToFit()
+        return textView
+    }()
+
+    weak var delegate: WriteTextViewCellDelegate?
+    private let paddingLeadingTrailing: CGFloat = 23.0
+    private let paddingTopBottom: CGFloat = 24.0
+    private let minHeight: CGFloat = 975.0
+
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+
+        setupView()
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+extension WriteTextViewCell {
+    private func setupView() {
+        selectionStyle = .none
+
+        contentView.addSubview(textView)
+
+        NSLayoutConstraint.activate([
+            textView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: paddingLeadingTrailing),
+            textView.topAnchor.constraint(equalTo: contentView.topAnchor, constant: paddingTopBottom),
+            textView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -paddingLeadingTrailing),
+            textView.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: -paddingTopBottom),
+            textView.heightAnchor.constraint(greaterThanOrEqualToConstant: minHeight)
+        ])
+
+        textView.delegate = self
+    }
+}
+
+extension WriteTextViewCell: UITextViewDelegate {
+    func textViewDidBeginEditing(_ textView: UITextView) {
+        if textView.textColor == SwiftGenColors.gray4.color {
+            textView.text = nil
+            textView.textColor = SwiftGenColors.black.color
+        }
+    }
+
+    func textViewDidEndEditing(_ textView: UITextView) {
+        if textView.text.isEmpty {
+            textView.text = "물건에 대한 생각이나 감정을 자유롭게 기록해보세요."
+            textView.textColor = SwiftGenColors.gray4.color
+        }
+    }
+
+    func textViewDidChange(_ textView: UITextView) {
+        if let delegate: WriteTextViewCellDelegate = delegate {
+            delegate.updateTextViewHeight(self, textView)
+        }
+    }
+}
