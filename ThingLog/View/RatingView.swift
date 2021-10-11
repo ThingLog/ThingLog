0e4ca4f245665feb83254e75b5fab6ced518e302
JiWon Lee <jiwon.swdev@gmail.com>
2021-10-11 15:46:56 +0900

    [#82] view: WriteRatingCell

---
+++ ThingLog/View/RatingView.swift
@@ -0,0 +1,73 @@
+//
+//  RatingView.swift
+//  ThingLog
+//
+//  Created by 이지원 on 2021/10/11.
+//  Source: https://ios-development.tistory.com/638
+
+import UIKit
+
+final class RatingView: UIView {
+    var maxCount: Int = 5 {
+        didSet { setupRatingButton() }
+    }
+
+    var currentRating: Int = 0
+    private var buttons: [UIButton] = []
+    private lazy var stackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.axis = .horizontal
+        stackView.distribution = .equalSpacing
+        stackView.spacing = 14
+        return stackView
+    }()
+
+    private let fillImage: UIImage = SwiftGenAssets.rating.image.withTintColor(SwiftGenColors.black.color)
+    private let emptyImage: UIImage = SwiftGenAssets.rating.image
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+
+        setupRatingButton()
+        setupView()
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+extension RatingView {
+    private func setupView() {
+        addSubview(stackView)
+
+        NSLayoutConstraint.activate([
+            stackView.leadingAnchor.constraint(equalTo: leadingAnchor),
+            stackView.topAnchor.constraint(equalTo: topAnchor),
+            stackView.trailingAnchor.constraint(equalTo: trailingAnchor),
+            stackView.bottomAnchor.constraint(equalTo: bottomAnchor)
+        ])
+    }
+
+    private func setupRatingButton() {
+        (0..<maxCount).forEach {
+            let button: UIButton = UIButton()
+            button.setImage(emptyImage, for: .normal)
+            button.tag = $0
+            buttons += [button]
+            stackView.addArrangedSubview(button)
+            button.addTarget(self, action: #selector(didTapButton), for: .touchUpInside)
+        }
+    }
+
+    @objc
+    private func didTapButton(_ sender: UIButton) {
+        let end: Int = sender.tag
+
+        (0...end).forEach { buttons[$0].setImage(fillImage, for: .normal) }
+        (end + 1..<maxCount).forEach { buttons[$0].setImage(emptyImage, for: .normal) }
+
+        currentRating = end + 1
+    }
+}
---
+++ ThingLog/View/TableVeiwCell/WriteRatingCell.swift
@@ -0,0 +1,64 @@
+//
+//  WriteRatingCell.swift
+//  ThingLog
+//
+//  Created by 이지원 on 2021/10/11.
+//
+
+import UIKit
+
+final class WriteRatingCell: UITableViewCell {
+    private let label: UILabel = {
+        let label: UILabel = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.textColor = SwiftGenColors.black.color
+        label.font = UIFont.Pretendard.body1
+        label.text = "만족도"
+        label.setContentHuggingPriority(.required, for: .horizontal)
+        return label
+    }()
+
+    private let ratingView: RatingView = {
+        let ratingView: RatingView = RatingView()
+        ratingView.translatesAutoresizingMaskIntoConstraints = false
+        return ratingView
+    }()
+
+    var currentRating: Int { ratingView.currentRating }
+    private let paddingLabelLeading: CGFloat = 31.0
+    private let paddingLabelTrailing: CGFloat = 60.0
+    private let paddingLabelTopBottom: CGFloat = 20.0
+    private let paddingRatingTopBottom: CGFloat = 18.0
+    private let paddingRatingTrailing: CGFloat = 46.0
+
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+
+        setupView()
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+extension WriteRatingCell {
+    private func setupView() {
+        selectionStyle = .none
+
+        contentView.addSubview(label)
+        contentView.addSubview(ratingView)
+
+        NSLayoutConstraint.activate([
+            // Rating Label
+            label.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: paddingLabelLeading),
+            label.topAnchor.constraint(equalTo: contentView.topAnchor, constant: paddingLabelTopBottom),
+            label.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: -paddingLabelTopBottom),
+            // Rating View
+            ratingView.leadingAnchor.constraint(equalTo: label.trailingAnchor, constant: paddingLabelTrailing),
+            ratingView.topAnchor.constraint(equalTo: contentView.topAnchor, constant: paddingRatingTopBottom),
+            ratingView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -paddingRatingTrailing),
+            ratingView.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: -paddingLabelTopBottom)
+        ])
+    }
+}
