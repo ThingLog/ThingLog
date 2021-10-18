ded56c95c768418f8be1bae9e053825804029493
JiWon Lee <jiwon.swdev@gmail.com>
2021-10-18 18:12:38 +0900

    [#82] chore: DisableSelectionTextField 주석 추가

--- ThingLog/View/DisableSelectionTextField.swift
+++ ThingLog/View/DisableSelectionTextField.swift
@@ -7,6 +7,8 @@
 
 import UIKit
 
+/// TextField의 Selection, Action을 끌 수 있는 서브 클래스
+/// 글쓰기 화면에서 WriteTextFieldCell에서 사용한다. 키보드 타입이 numberpad 인 경우, isSelection을 false로 변경해 Selection, Action을 사용하지 않는다.
 final class DisableSelectionTextField: UITextField {
     var isSelection: Bool = true
 
