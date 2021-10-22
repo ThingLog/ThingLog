15773fa572befd3efc240ec86246fa4ff4859d45
JiWon Lee <jiwon.swdev@gmail.com>
2021-10-22 18:21:10 +0900

    [#32] view: UIAlertViewController를 AlertViewController로 변경

--- ThingLog/ViewController/WriteViewController.swift
+++ ThingLog/ViewController/WriteViewController.swift
@@ -17,13 +17,13 @@ final class WriteViewController: UIViewController {
         let tableView: UITableView = UITableView(frame: .zero, style: .plain)
         tableView.translatesAutoresizingMaskIntoConstraints = false
         tableView.separatorInset = UIEdgeInsets(top: 0, left: 16, bottom: 0, right: 16)
-
+        
         let headerLabel: UILabel = UILabel(frame: CGRect(x: 0, y: 0, width: tableView.frame.size.width, height: 29.0))
         headerLabel.font = UIFont.Pretendard.body2
         headerLabel.textColor = SwiftGenColors.gray3.color
         headerLabel.text = "\(Date().toString(.year))년 \(Date().toString(.month))월 \(Date().toString(.day))일"
         headerLabel.textAlignment = .center
-
+        
         tableView.tableHeaderView = headerLabel
         tableView.tableFooterView = UIView(frame: CGRect(x: 0, y: 0, width: tableView.frame.size.width, height: 1))
         if #available(iOS 15.0, *) {
@@ -31,50 +31,54 @@ final class WriteViewController: UIViewController {
         }
         return tableView
     }()
-
+    
     let doneButton: CenterTextButton = {
         let button: CenterTextButton = CenterTextButton(buttonHeight: 54.0, title: "작성 완료")
         button.translatesAutoresizingMaskIntoConstraints = false
         return button
     }()
-
+    
     // MARK: - Properties
     var coordinator: Coordinator?
     var viewModel: WriteViewModel?
     let disposeBag: DisposeBag = DisposeBag()
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = SwiftGenColors.white.color
-
+        
         setupNavigationBar()
         setupView()
         setupBind()
     }
-
+    
     @objc
     /// 글쓰기 화면을 닫는다.
     /// 글쓰기 화면을 닫기 전에 alert 팝업을 띄워 다시 한 번 사용자에게 묻는다.
     /// 글쓰기 화면을 닫으면서 navigationController.viewControllers를 초기화한다.
     func close() {
-        let alertController: UIAlertController = UIAlertController(title: "Title Here",
-                                                                   message: "Alert description",
-                                                                   preferredStyle: .alert)
-
-        let cancleAction: UIAlertAction = UIAlertAction(title: "cancle",
-                                                        style: .cancel,
-                                                        handler: nil)
-        let saveAction: UIAlertAction = UIAlertAction(title: "save",
-                                                      style: .default,
-                                                      handler: { [weak self] _ in
-            self?.navigationController?.dismiss(animated: true, completion: {
-                self?.navigationController?.viewControllers.removeAll()
-            })
-        })
-
-        alertController.addAction(cancleAction)
-        alertController.addAction(saveAction)
-        present(alertController, animated: true, completion: nil)
+        let alertController: AlertViewController = AlertViewController()
+        
+        alertController.hideTitleLabel()
+        alertController.changeContentsText("글이 저장되지 않고 삭제됩니다.\n나가시겠어요?")
+        alertController.hideTextField()
+        alertController.leftButton.setTitle("아니요", for: .normal)
+        alertController.rightButton.setTitle("네", for: .normal)
+        
+        alertController.leftButton.rx.tap.bind {
+            alertController.dismiss(animated: false, completion: nil)
+        }.disposed(by: disposeBag)
+        
+        alertController.rightButton.rx.tap.bind { [weak self] in
+            alertController.dismiss(animated: false) {
+                self?.navigationController?.dismiss(animated: true, completion: {
+                    self?.navigationController?.viewControllers.removeAll()
+                })
+            }
+        }.disposed(by: disposeBag)
+        
+        alertController.modalPresentationStyle = .overFullScreen
+        present(alertController, animated: false, completion: nil)
     }
 }
 
@@ -83,34 +87,34 @@ extension WriteViewController: UITableViewDataSource {
     func numberOfSections(in tableView: UITableView) -> Int {
         WriteViewModel.Section.allCases.count
     }
-
+    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         viewModel?.itemCount[section] ?? 0
     }
-
+    
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let section: WriteViewModel.Section? = .init(rawValue: indexPath.section)
-
+        
         switch section {
         case .image:
             guard let cell: WriteImageTableCell = tableView.dequeueReusableCell(withIdentifier: WriteImageTableCell.reuseIdentifier, for: indexPath) as? WriteImageTableCell else {
                 return UITableViewCell()
             }
-
+            
             return cell
         case .type:
             guard let cell: WriteTextFieldCell = tableView.dequeueReusableCell(withIdentifier: WriteTextFieldCell.reuseIdentifier, for: indexPath) as? WriteTextFieldCell else {
                 return WriteTextFieldCell()
             }
-
+            
             cell.keyboardType = viewModel?.typeInfo[indexPath.row].keyboardType ?? .default
             cell.placeholder = viewModel?.typeInfo[indexPath.row].placeholder
-
+            
             cell.isEditingSubject
                 .bind { _ in
                     tableView.scrollToRow(at: indexPath, at: .top, animated: true)
                 }.disposed(by: cell.disposeBag)
-
+            
             return cell
         case .rating:
             guard let cell: WriteRatingCell = tableView.dequeueReusableCell(withIdentifier: WriteRatingCell.reuseIdentifier, for: indexPath) as? WriteRatingCell else {
@@ -121,14 +125,14 @@ extension WriteViewController: UITableViewDataSource {
             guard let cell: WriteTextViewCell = tableView.dequeueReusableCell(withIdentifier: WriteTextViewCell.reuseIdentifier, for: indexPath) as? WriteTextViewCell else {
                 return WriteTextViewCell()
             }
-
+            
             cell.delegate = self
-
+            
             cell.isEditingSubject
                 .bind { _ in
                     tableView.scrollToRow(at: indexPath, at: .top, animated: true)
                 }.disposed(by: cell.disposeBag)
-
+            
             return cell
         default:
             return UITableViewCell()
