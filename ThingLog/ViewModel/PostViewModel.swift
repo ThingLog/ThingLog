f54dbaf96d235e10f55fac2e72f9f16f548a4625
JiWon Lee <jiwon.swdev@gmail.com>
2021-11-02 17:00:36 +0900

    [#170] docs: 주석 추가

--- ThingLog/ViewModel/PostViewModel.swift
+++ ThingLog/ViewModel/PostViewModel.swift
@@ -11,7 +11,9 @@ protocol PostViewModelProtocol: AnyObject {
     associatedtype Screen
 
     var posts: [Post] { get set }
+    /// 데이터 표시 시작 지점을 위한 프로퍼티
     var startIndexPath: IndexPath { get set }
+    /// 어느 화면에서 표시하는 지 알기 위한 프로퍼티
     var screen: Screen { get set }
 
     init(posts: [Post], startIndexPath: IndexPath, from screen: Screen)
