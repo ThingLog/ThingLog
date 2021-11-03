03f1bce90e889e32e04f7adf70e9116237c39432
JiWon Lee <jiwon.swdev@gmail.com>
2021-11-03 12:37:34 +0900

    [#170] docs: PostCoordinatorProtocol 주석 추가

--- ThingLog/Coordinator/PostCoordinator.swift
+++ ThingLog/Coordinator/PostCoordinator.swift
@@ -8,5 +8,6 @@
 import Foundation
 
 protocol PostCoordinatorProtocol: Coordinator {
+    /// `PostViewController`를 보여준다. `PostViewController`에서 데이터를 표시하기 위해 `PostViewModel`이 필요하다.
     func showPostViewController(with viewModel: PostViewModel)
 }
