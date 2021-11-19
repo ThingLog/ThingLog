c0dfe48babfa78d2cdaae2c3b81100fd4a5f0ed1
gustn3965 <gustn3965@gmail.com>
2021-11-19 17:13:41 +0900

    [#207] feat: 포토카드 나머지 기능 구현 - 포토카드 저장할 경우 인의예지상 획득 호출 - 권한 취소 -> 팝업 뜯도록 구현 - PhotoCardCoordinator 구현 - 실제 데이터 바인딩

--- ThingLog/Coordinator/PhotoCardCoordinator.swift
+++ ThingLog/Coordinator/PhotoCardCoordinator.swift
@@ -8,8 +8,9 @@
 import Foundation
 import UIKit.UIImage
 
-protocol PhotoCardCoordinatorProtocol: Coordinator {
+protocol PhotoCardCoordinatorProtocol: SystemSettingCoordinatorProtocol {
     /// `PhotoCardViewController`를 보여준다. `PhotoCardViewController`에서 데이터를 표시하기 위해 `PostEntity`와 특정 이미지`UIImage`가 필요하다.
     func showPhotoCardController(post: PostEntity,
                                  image: UIImage)
+    
 }
