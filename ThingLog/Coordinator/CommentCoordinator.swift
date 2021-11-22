d719bbe388dafd47a6af925198b267642e0c177e
JiWon Lee <jiwon.swdev@gmail.com>
2021-11-22 10:59:02 +0900

    [#234] feat: CommentCoordinatorProtocol 정의

---
+++ ThingLog/Coordinator/CommentCoordinator.swift
@@ -0,0 +1,14 @@
+//
+//  CommentCoordinator.swift
+//  ThingLog
+//
+//  Created by 이지원 on 2021/11/22.
+//
+
+import Foundation
+
+/// 댓글 화면으로 이동하기 위한 메소드를 정의한 Coordinator 프로토콜
+protocol CommentCoordinatorProtocol {
+    /// `CommentViewController`로 이동한다. `CommentViewController`에서 데이터를 표시하기 위해 `PostEntity`가 필요하다.
+    func showCommentViewController(with entity: PostEntity)
+}
