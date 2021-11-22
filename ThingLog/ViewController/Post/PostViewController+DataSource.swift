789afc8e542a0a74df8a41cda04b27e3fde89b72
JiWon Lee <jiwon.swdev@gmail.com>
2021-11-22 15:39:48 +0900

    [#236] feat: 게시물 좋아요 기능 구현

--- ThingLog/ViewController/Post/PostViewController+DataSource.swift
+++ ThingLog/ViewController/Post/PostViewController+DataSource.swift
@@ -17,9 +17,23 @@ extension PostViewController: UITableViewDataSource {
             return PostTableCell()
         }
 
+        let repository: PostRepository = PostRepository(fetchedResultsControllerDelegate: nil)
         let item: PostEntity = viewModel.fetchedResultsController.object(at: indexPath)
+
         cell.configure(with: item)
-        
+        cell.likeButton.rx.tap
+            .bind {
+                item.isLike.toggle()
+                repository.update(item) { result in
+                    switch result {
+                    case .success:
+                        cell.likeButton.isSelected = item.isLike
+                    case .failure(let error):
+                        fatalError("\(#function): \(error.localizedDescription)")
+                    }
+                }
+            }.disposed(by: cell.disposeBag)
+
         return cell
     }
 }
