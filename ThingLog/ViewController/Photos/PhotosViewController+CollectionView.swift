6dc3d1300430e4170ed29d1b572aca970e05981e
JiWon Lee <jiwon.swdev@gmail.com>
2021-11-26 20:49:18 +0900

    [#157] ref: 카메라 권한 요청 코드 수정 - imagePickerController를 present 함으로써 받는 권한 요청은, 사용자가 취소를 눌렀을 때도 이미 카메라 화면이 보여지고 있기 때문에 해당 부분은 변경

--- ThingLog/ViewController/Photos/PhotosViewController+CollectionView.swift
+++ ThingLog/ViewController/Photos/PhotosViewController+CollectionView.swift
@@ -83,9 +83,21 @@ extension PhotosViewController: UICollectionViewDataSource {
 extension PhotosViewController: UICollectionViewDelegate {
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
         if indexPath.item == 0 {
-            if AVCaptureDevice.authorizationStatus(for: .video) == .authorized {
+            switch AVCaptureDevice.authorizationStatus(for: .video) {
+            case .authorized:
                 present(imagePickerController, animated: true)
-            } else {
+            case .notDetermined:
+                AVCaptureDevice.requestAccess(for: .video) { [weak self] granted in
+                    guard let self = self else { return }
+                    if granted {
+                        self.present(self.imagePickerController, animated: true)
+                    }
+                }
+            case .denied:
+                coordinator?.showMoveSettingAlert()
+            case .restricted:
+                coordinator?.showMoveSettingAlert()
+            @unknown default:
                 coordinator?.showMoveSettingAlert()
             }
         }
