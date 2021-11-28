1bc76abd369e9b7712c48d924d2428c1db431a93
gustn3965 <gustn3965@gmail.com>
2021-11-28 12:38:55 +0900

    [#249] ref: 네비게이션 뷰 백 스와이프 가능

---
+++ ThingLog/Extension/UINavigationController+.swift
@@ -0,0 +1,18 @@
+//
+//  UINavigationController+.swift
+//  ThingLog
+//
+//  Created by hyunsu on 2021/11/28.
+//
+
+import UIKit
+
+extension UINavigationController: ObservableObject, UIGestureRecognizerDelegate {
+    open override func viewDidLoad() {
+        super.viewDidLoad()
+        interactivePopGestureRecognizer?.delegate = self
+    }
+    public func gestureRecognizerShouldBegin(_ gestureRecognizer: UIGestureRecognizer) -> Bool {
+        viewControllers.count > 1
+    }
+}
