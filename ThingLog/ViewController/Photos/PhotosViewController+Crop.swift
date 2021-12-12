680c919e919dbe476309d34e167db0666086f910
gustn3965 <gustn3965@gmail.com>
2021-12-12 19:44:41 +0900

    [#283] feat: 크롭뷰 - 1:1크롭 기능 없애고, 상단 우측 하단의 초록색 숫자 뷰 없애기.

--- ThingLog/ViewController/Photos/PhotosViewController+Crop.swift
+++ ThingLog/ViewController/Photos/PhotosViewController+Crop.swift
@@ -11,7 +11,7 @@ extension PhotosViewController {
     /// 오른쪽에서 왼쪽으로 나타나는 애니메이션과 함께 CropViewController를 표시한다.
     func showCropViewController(selectedIndexImage: (index: IndexPath, image: UIImage?)) {
         cropViewController = CropViewController(selectedIndexImage: selectedIndexImage)
-        cropViewController?.numberView.label.text = "\(selectedIndexPath.count)"
+//        cropViewController?.numberView.label.text = "\(selectedIndexPath.count)" // 기능 없애기로 함.
         cropViewController?.backCompletion = dismissCropViewController
         guard let cropViewController: CropViewController = cropViewController else { return }
         let cropView: UIView = cropViewController.view
